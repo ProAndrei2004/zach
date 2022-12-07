44287f213409ec72a7eebbd08a0e207281445d77
305-5 <305-5@305-5>
2022-12-07 13:37:52 +0500

    Добавьте файлы проекта.

---
+++ zach/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace zach
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Button_Clicked(object sender, EventArgs e)
+        {
+            if (login.Text != "" && password.Text != "")
+            {
+                await Navigation.PushAsync(new Page1());
+            }
+            else
+            {
+                await DisplayAlert("Внимание", "Поля ввода пустые", "OK");
+            }
+        }
+    }
+}
---
+++ zach/Page1.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace zach
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        async void ImageButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            double slid = slider.Value;
+            
+            string srok;
+            srok = credit.Text;
+            double srok1 = Convert.ToDouble(srok);
+            double a;
+
+            if(srok1 <= 12)
+            {     
+                EveryMounthPay.Text = $"{slid / srok1 + slid * 0.059}";
+            }
+            else if(srok1 > 13 && srok1 <= 24)
+            {
+                double dogoda = slid / srok1 + slid * 0.059;
+                EveryMounthPay.Text = $"{(dogoda + slid) / srok1 + (slid - 6000) * 0.059}";
+            }
+            else
+            {
+                double dogoda = slid / srok1 + slid * 0.059;
+                EveryMounthPay.Text = $"{(dogoda + slid) / srok1 + (slid - 10000)* 0.042}";
+            }
+
+
+
+        }
+
+        private void slider_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+
+        }
+    }
+}
